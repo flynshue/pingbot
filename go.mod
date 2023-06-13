7e4ed62f06690621c3fa392465a0d4eb876c317b
Felicia Lyn-Shue <flynshue@gmail.com>
2023-06-13 12:00:40 -0400

    pingbot with custom responsewriter

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/flynshue/pingbot
 go 1.15
 
 require (
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rs/zerolog v1.29.1
 	github.com/shomali11/proper v0.0.0-20190608032528-6e70a05688e7 // indirect
-	github.com/shomali11/slacker v0.0.0-20211215041426-c14027d091d5
-	github.com/slack-go/slack v0.10.0 // indirect
+	github.com/shomali11/slacker v1.4.1
+	github.com/slack-go/slack v0.12.2 // indirect
+	github.com/spf13/viper v1.15.0
 )
--- main.go
+++ main.go
@@ -2,27 +2,89 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/shomali11/slacker"
+	"github.com/spf13/viper"
 )
 
+var cfgFile = os.Getenv("PINGBOT_CONFIG_FILE")
+
 func main() {
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	c := make(chan int)
+	go startBot()
+	go refreshToken()
+	<-c
+}
+
+func startBot() {
+	bot := slacker.NewClient(viper.GetString("bot-token"), viper.GetString("app-token"))
 	definition := &slacker.CommandDefinition{
-		Description: "ping commnad",
-		Example:     "ping",
+		Description: "ping command",
+		Examples:    []string{"ping"},
 		Handler:     pingHandler,
 	}
+	echoDefinition := &slacker.CommandDefinition{
+		Description: "echo command",
+		Examples:    []string{"echo <sentence>"},
+		Handler:     echoHandler,
+	}
 	bot.Command("ping", definition)
+	bot.Command("echo", echoDefinition)
+	bot.CustomResponse(NewResponse)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	if err := bot.Listen(ctx); err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err)
 	}
 }
 
+func echoHandler(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+	log.Printf("echo logs only in debug mode")
+	response.Reply(fmt.Sprintf("%s from %s", botCtx.Event().Text, botCtx.Event().UserProfile.Email))
+}
+
 func pingHandler(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-	response.Reply("pong")
+	user := botCtx.Event().UserProfile
+	reqMsg := botCtx.Event().Text
+	log.Info().Msgf("%s from %s", reqMsg, user.Email)
+	response.Reply(user.Email)
+}
+
+func refreshToken() {
+	for {
+		log.Printf("refresh token goes here.")
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func init() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pingbot")
+		viper.SetConfigType("yaml")
+	}
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+	debug := viper.GetBool("debug")
+	log.Info().Msgf("%v", debug)
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	if err := viper.ReadInConfig(); err == nil {
+		log.Printf("Using config file: %s\n", viper.ConfigFileUsed())
+	}
 }
